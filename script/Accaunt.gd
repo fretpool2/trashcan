7de075abfb9094ff8cf4317faa8ff5bb7e4c6b0b
fretpool2 <ilaimaksmm17@gmail.com>
2024-01-22 15:38:02 +0300

    1

---
+++ script/Accaunt.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var login : String
+var password : String
+var firstName : String
+var LastName : String
+var mail : String
---
+++ script/main/Main.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _on_loginin_pressed():
+	get_tree().change_scene_to_file("res://scene/main/auth.tscn")
+
+
+
+func _on_reg_pressed():
+	get_tree().change_scene_to_file("")
