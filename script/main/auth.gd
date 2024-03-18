2550d15cb6f0bd2c27b795270822a38b7e36e643
fretpool2 <ilaimaksmm17@gmail.com>
2024-03-18 20:29:06 +0300

    Update ui/ux main_manu
    Update ui/ux main_manu

--- script/main/auth.gd
+++ script/main/auth.gd
@@ -1,10 +1,17 @@
 extends Control
 
+
+@onready var animation_player = $AnimationPlayer
 @onready var log = $MarginContainer/VBoxContainer2/HBoxContainer3/login
 @onready var pas = $MarginContainer/VBoxContainer2/HBoxContainer/pasw
 @onready var check_pole = $MarginContainer/VBoxContainer2/check_pole
 @onready var label = $MarginContainer/VBoxContainer2/check_pole/check_pole
+@onready var next = $next
+@onready var next_2 = $next2
 var falseAcc = false
+
+func _ready():
+	animation_player.play("start")
 
 func check():
 	if log.text == "" or pas.text == "":
@@ -21,16 +28,24 @@ func check():
 
 func _on_button_pressed():
 	if log.text == Accaunt.login and pas.text == Accaunt.password:
+		animation_player.play("next")
 		check_pole.hide()
 		print("Logining")
 		Accaunt.signin = true
-		get_tree().change_scene_to_file("res://scene/pages/MainMenu/main_menu.tscn")
+		next_2.start()
 		Accaunt.save_acc_data()
 	else:
 		falseAcc = true
 		print("False")
 	check()
 
-
 func _on_no_acc_2_pressed():
+	animation_player.play("next")
+	next.start()
+
+func _on_next_timeout():
 	get_tree().change_scene_to_file("res://scene/main/registr.tscn")
+
+
+func _on_next_2_timeout():
+	get_tree().change_scene_to_file("res://scene/pages/MainMenu/main_menu.tscn")
--- script/main_menu/main_menu.gd
+++ script/main_menu/main_menu.gd
@@ -1,5 +1,10 @@
 extends Control
 
+@onready var names = $MarginContainer/VBoxContainer2/VBoxContainer3/HBoxContainer3/Name
+
+func _ready():
+	names.text = Accaunt.firstName + "!"
+	pass
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scene/main/main.tscn")
 
