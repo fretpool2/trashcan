57843caa26ddc3b66473611d33c4fc1f2979ff76
fretpool2 <ilaimaksmm17@gmail.com>
2024-04-05 11:18:41 +0300

    Udaptatiom

--- script/profile/SetingsProfile.gd
+++ script/profile/SetingsProfile.gd
@@ -1,13 +1,2 @@
 extends Control
 
-@onready var changeName = $MarginContainer/VBoxContainer/changeName/changeName
-@onready var changePassword = $MarginContainer/VBoxContainer/changePasword/changePasword
-@onready var lastName = $MarginContainer/VBoxContainer/lastName/lastName
-
-func _on_save_pasword_pressed():
-	Accaunt.password = changePassword.text
-	Accaunt.firstName = changeName.text
-	Accaunt.lastName = lastName.text
-	
-func _on_exit_pressed():
-	get_tree().change_scene_to_file("res://scene/pages/MainMenu/main_menu.tscn")
